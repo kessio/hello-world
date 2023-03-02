4a6a8cbf357a64aeed0378a809b1770441062483
unknown <sharonkessio6@gmail.com>
2023-03-02 05:55:45 +0300

    setup babale, db, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const webpack = require("webpack")
 module.exports = {
   mode: "production",
   devtool: "source-map",
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   entry: {
     application: "./app/javascript/application.js"
   },
@@ -12,6 +15,15 @@ module.exports = {
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },  
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
