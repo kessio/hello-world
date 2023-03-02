bc12eb69e1d7d851ea8b8e81d08e98f546711953
unknown <sharonkessio6@gmail.com>
2023-03-02 18:49:42 +0300

    fix rubocop erros

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def random_greetings
-    greeting = Greeting.order("RANDOM()").first
+    greeting = Greeting.order('RANDOM()').first
     render json: { message: greeting.text }
   end
 end
